24f87d5885695c93e42213efec2540ab38a09887
Manolis Chalkiadakis <manos.ch96@gmail.com>
2019-10-26 22:50:50 +0300

    Split into separate classes

--- Localization/Localization.py
+++ Localization/Localization.py
@@ -5,21 +5,22 @@ import json
 
 _LOGGER = logging.getLogger(__name__)
 
-
-class LocalizationServer(object):
-    """ Class handling requests """
+class Server:
+    """ Class initializing server """
 
     def __init__(self, port=2000):
 
         self.loop = asyncio.get_event_loop()
-        listen = self.loop.create_datagram_endpoint(LocalizationHandler, local_addr=('127.0.0.1', port))
+        listen = self.loop.create_datagram_endpoint(Handler, local_addr=('127.0.0.1', port))
         self. loop.run_until_complete(listen)
         self.loop.run_forever()
 
         _LOGGER.debug("Server up at port %s", port)
 
 
-class LocalizationHandler:
+class Handler:
+    """ Class handling requests """
+
     def connection_made(self, transport):
         self.transport = transport
 
@@ -27,8 +28,33 @@ class LocalizationHandler:
         message = data.decode().strip('\n')
         _LOGGER.debug('Received from %s message "%s"', addr, message)
         try:
-            _LOGGER.debug(json.loads(data))
+            jsondata = json.loads(data)
         except Exception as e:
             _LOGGER.error("NO JSON: %s", e)
+            return
         # print('Send %r to %s' % (message, addr))
         # self.transport.sendto(data, addr)
+
+class Mapper:
+    """ Class handling data """
+
+    def __init__(self):
+        self.data = {}
+
+    def write_map(self, mac, rssi, data):
+        if mac not in self.data:
+            self.data[mac] = {}
+        if rssi not in self.data[mac]:
+            self.data[mac] = []
+        if data not in self.data[mac][rssi]:
+            self.data[mac][rssi].append(data)
+
+    def write_json_map(self, dict_map, data):
+        for mac, rssi in dict_map:
+            self.add_map(mac, rssi, data)
+
+    def read(self, dict_map):
+        data = []
+        for mac, rssi in dict_map:
+            data.append(self.data[mac][rssi])
+
