9b31ded4974cba873754641b47985c80f97a8578
Manolis Chalkiadakis <manos.ch96@gmail.com>
2019-11-17 19:06:16 +0200

    example

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 import Localization
 from Localization import Mapper
+from Localization import Thinker
 import logging
 
 logger = logging.getLogger('Localization.Localization')
@@ -8,13 +9,30 @@ handler = logging.StreamHandler()
 logger.addHandler(handler)
 
 
-m.remove_room(3)
-server = Localization.Server(m)
-server.add_write_server()
-server.looper()
+def training():
+    t = Thinker.Thinker(
+        read_macs="/tmp/thinker_macs.csv", read_filename="/tmp/thinker.csv",
+        write_macs="/tmp/thinker_macs.csv", write_filename="/tmp/thinker.csv"
+    )
+    t.train(outfile="/tmp/trained")
+
+def server_thinker():
+    t = Thinker.Thinker(
+        read_macs="/tmp/thinker_macs.csv", read_filename="/tmp/thinker.csv",
+        write_macs="/tmp/thinker_macs.csv", write_filename="/tmp/thinker.csv",
+        model_file="/tmp/trained"
+    )
+    s = Localization.Server(t)
+    s.add_write_server()
+    s.looper()
+
 def server_mapper():
     m = Mapper.Mapper(read_filename="/tmp/mapper.csv", write_filename="/tmp/mapper.csv")
     s = Localization.Server(m)
     s.add_write_server()
     s.looper()
-server_mapper()
+
+# t.load_from_mapper(m)
+training()
+server_thinker()
+# server_mapper()
